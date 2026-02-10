05013a6aca17cb5687e4706fdcd7ed7a7d162ce3
Laura Jimenez <laufjimdev@gmail.com>
2026-02-10 15:12:21 -0500

    Deploy update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio-site/'
+  base: "/portfolio-site/",
+  plugins: [react()]
 })
